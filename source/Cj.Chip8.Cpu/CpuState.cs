724b410546ba1c91b5d1314dc24871cfdb84ec82
Chris Jansson <chris.janssom@gmail.com>
2013-01-06 14:46:31 +0100

    Added hex sprites to memory

--- source/Cj.Chip8.Cpu/CpuState.cs
+++ source/Cj.Chip8.Cpu/CpuState.cs
@@ -1,7 +1,34 @@
-﻿namespace Cj.Chip8.Cpu
+﻿using System;
+
+namespace Cj.Chip8.Cpu
 {
     public class CpuState
     {
+        public CpuState()
+        {
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x90, 0x90, 0x90, 0xF0 }, 0);
+            AddHexaDecimalDigit(new byte[] { 0x20, 0x60, 0x20, 0x20, 0x70 }, 5);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x10, 0xF0, 0x80, 0xF0 }, 10);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x10, 0xF0, 0x10, 0xF0 }, 15);
+            AddHexaDecimalDigit(new byte[] { 0x90, 0x90, 0xF0, 0x10, 0x10 }, 20);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x80, 0xF0, 0x10, 0xF0 }, 25);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x80, 0xF0, 0x90, 0xF0 }, 30);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x10, 0x20, 0x40, 0x40 }, 35);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x90, 0xF0, 0x90, 0xF0 }, 40);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x90, 0xF0, 0x10, 0xF0 }, 45);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x90, 0xF0, 0x90, 0x90 }, 50);
+            AddHexaDecimalDigit(new byte[] { 0xE0, 0x90, 0xE0, 0x90, 0xE0 }, 55);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x80, 0x80, 0x80, 0xF0 }, 60);
+            AddHexaDecimalDigit(new byte[] { 0xE0, 0x90, 0x90, 0x90, 0xE0 }, 65);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x80, 0xF0, 0x80, 0xF0 }, 70);
+            AddHexaDecimalDigit(new byte[] { 0xF0, 0x80, 0xF0, 0x80, 0x80 }, 75);
+        }
+
+        private void AddHexaDecimalDigit(byte[] digit, int memoryOffset)
+        {
+            Array.Copy(digit, 0, Memory, memoryOffset, digit.Length);
+        }
+
         public readonly byte[] Memory = new byte[4096];
         public short ProgramCounter = 0;
         public byte StackPointer;
