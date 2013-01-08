a9daa66fefc64a9ec511111f9bb78a969c6bc4f5
Chris Jansson <chris.janssom@gmail.com>
2013-01-08 20:50:02 +0100

    Some instruction decoding

--- source/Cj.Chip8.Cpu/InstructionDecoder.cs
+++ source/Cj.Chip8.Cpu/InstructionDecoder.cs
@@ -4,7 +4,28 @@
     {
         public void DecodeAndExecute(short instruction, IChip8Cpu cpu)
         {
-            
+            switch (instruction & 0xF000)
+            {
+                case 0x0000:
+                    switch (instruction)
+                    {
+                        case 0x00E0:
+                            cpu.Cls();
+                            break;
+                        case 0x00EE:
+                            cpu.Ret();
+                            break;
+                    }
+                    break;
+                case 0x1000:
+                    var jump = (short)(instruction & 0x0FFF);
+                    cpu.Jump(jump);
+                    break;
+                case 0x2000:
+                    var call = (short) (instruction & 0x0FFF);
+                    cpu.Call(call);
+                    break;
+            }
         }
     }
 }
--- source/Cj.Chip8.Test/InstructionDecoderTests.cs
+++ source/Cj.Chip8.Test/InstructionDecoderTests.cs
@@ -1,4 +1,8 @@
-﻿using Cj.Chip8.Cpu;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Cj.Chip8.Cpu;
 using Moq;
 using NUnit.Framework;
 
@@ -13,7 +17,7 @@ namespace Cj.Chip8.Test
         [SetUp]
         public void SetUp()
         {
-            _cpu = new Mock<IChip8Cpu>();
+            _cpu = new Mock<IChip8Cpu>(MockBehavior.Strict);
 
             _instructionDecoder = new InstructionDecoder();
         }
@@ -21,7 +25,65 @@ namespace Cj.Chip8.Test
         [Test]
         public void Should_call_CLS_when_decoding_00E0()
         {
-            
+            const short instruction = 0x00E0;
+
+            ExecuteAndVerify(instruction, x => x.Cls());
+        }
+
+        [Test]
+        public void Should_call_RET_when_decoding_00EE()
+        {
+            const short instruction = 0x00EE;
+
+            ExecuteAndVerify(instruction, x => x.Ret());
+        }
+
+        [Test]
+        public void Should_call_JMP_when_decoding_1nnn()
+        {
+            var instructions = CreateRange(0xFFF)
+                .Select(x => (short)x)
+                .ToList();
+
+            foreach (var targetAddress in instructions)
+            {
+                var address = targetAddress;
+
+                var instruction = (short)(0x1000 | targetAddress);
+
+                ExecuteAndVerify(instruction, x => x.Jump(address));
+            }
+        }
+
+        [Test]
+        public void Should_call_CALL_when_decoding_2nnn()
+        {
+            var instructions = CreateRange(0xFFF)
+                .Select(x => (short)x)
+                .ToList();
+
+            foreach (var targetAddress in instructions)
+            {
+                var address = targetAddress;
+
+                var instruction = (short)(0x2000 | targetAddress);
+
+                ExecuteAndVerify(instruction, x => x.Call(address));
+            }
+        }
+
+        private void ExecuteAndVerify(short instruction, Expression<Action<IChip8Cpu>> verifier)
+        {
+            _cpu.Setup(verifier);
+
+            _instructionDecoder.DecodeAndExecute(instruction, _cpu.Object);
+
+            _cpu.Verify(verifier);
+        }
+
+        private IEnumerable<int> CreateRange(int maxValue)
+        {
+            return Enumerable.Range(0, maxValue + 1);
         }
     }
 }
