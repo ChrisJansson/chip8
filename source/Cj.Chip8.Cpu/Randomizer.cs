28582e0088615c1198ec006e3056c760c9c4231e
Chris Jansson <chris.janssom@gmail.com>
2013-01-13 15:26:59 +0100

    Implemented randomizer

---
+++ source/Cj.Chip8.Cpu/Randomizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cj.Chip8.Cpu
+{
+    public class Randomizer : IRandomizer
+    {
+        private readonly Random _random = new Random();
+        
+        public byte GetNext()
+        {
+            return (byte) _random.Next(0, 256);
+        }
+    }
+}
