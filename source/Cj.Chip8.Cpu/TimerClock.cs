30ecc2510815a237f64f5c8ec4d079b5b4a3f257
Chris Jansson <chris.janssom@gmail.com>
2013-01-19 14:58:58 +0100

    Implemented timers properly

---
+++ source/Cj.Chip8.Cpu/TimerClock.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Cj.Chip8.Cpu
+{
+    public class TimerClock : ITimerClock
+    {
+        private readonly Stopwatch _stopwatch;
+
+        public TimerClock()
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public double ElapsedSeconds
+        {
+            get { return _stopwatch.Elapsed.TotalSeconds; }
+        }
+
+        public void Reset()
+        {
+            _stopwatch.Restart();
+        }
+    }
+}
